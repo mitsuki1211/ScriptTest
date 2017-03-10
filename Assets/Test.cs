bb896ac786285b42c41e23eab59f33c9ba83d28e
森田実希 <mitsuki19951211@gmail.com>
2017-03-10 13:40:59 +0900

    ScriptTestプロジェクト　初めてのコミット
    スクリプトの課題と発展課題のファイルです。

---
+++ Assets/Test.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss
+{
+    private int hp = 100;          // 体力
+    private int power = 25; // 攻撃力
+    private int mp = 53; //魔法攻撃力
+
+    // 攻撃用の関数
+    public void Attack()
+    {
+        Debug.Log(this.power + "のダメージを与えた");
+    }
+
+    // 防御用の関数
+    public void Defence(int damage)
+    {
+        Debug.Log(damage + "のダメージを受けた");
+        // 残りhpを減らす
+        this.hp -= damage;
+    }
+
+    //魔法攻撃用の関数
+    public void Magic() {
+        if (this.mp >= 5)
+        {
+            this.mp -= 5;
+            Debug.Log("魔法攻撃をした。残りMPは" + this.mp + "。");
+        }
+        else
+        {
+            Debug.Log("MPが足りないため魔法が使えない。");
+        }
+    }
+}
+
+public class Test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        int[] array = {10,30,50,20,0};
+        int i = 0;
+
+        Debug.Log(array[0]);
+        Debug.Log(array[1]);
+        Debug.Log(array[2]);
+        Debug.Log(array[3]);
+        Debug.Log(array[4]);
+
+        for (i = 4; i >= 0; i--) {
+            Debug.Log(array[i]);
+        }
+
+        // Bossクラスの変数を宣言してインスタンスを代入
+        Boss lastboss = new Boss();
+
+        // 攻撃用の関数を呼び出す
+        lastboss.Attack();
+        // 防御用の関数を呼び出す
+        lastboss.Defence(3);
+
+        for (i = 0; i < 11; i++)
+        {
+            lastboss.Magic();
+        }
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
